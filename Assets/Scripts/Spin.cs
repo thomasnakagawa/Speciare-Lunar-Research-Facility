341cdd84bc126f618d7bb6ac5f3e438a4874be60
Thomas Nakagawa <thomasnakagawa@gmail.com>
2019-11-26 18:33:46 -0800

    Add music and title screen

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField] private float SpinSpeed;
+
+    private void Start()
+    {
+        StartSpinning();
+    }
+
+    public void StartSpinning()
+    {
+        StartCoroutine(SpinSelf());
+    }
+
+    private IEnumerator SpinSelf()
+    {
+        while (true)
+        {
+            transform.Rotate(Vector3.up * SpinSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
