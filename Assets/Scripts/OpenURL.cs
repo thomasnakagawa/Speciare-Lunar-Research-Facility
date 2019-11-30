a192fe7a1b173e565b0243be05c0a9e4888976cf
Thomas Nakagawa <thomasnakagawa@gmail.com>
2019-11-29 21:39:33 -0800

    Add urls to about content

---
+++ Assets/Scripts/OpenURL.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using OatsUtil;
+
+[RequireComponent(typeof(Button))]
+public class OpenURL : MonoBehaviour
+{
+    [SerializeField] private string URL = default;
+
+    void Start()
+    {
+        this.RequireComponent<Button>().onClick.AddListener(() =>
+        {
+            Application.OpenURL(URL);
+        });
+    }
+}
