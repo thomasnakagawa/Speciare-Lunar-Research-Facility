560f8148944de645ec4cbbb8a9c75a712eccca76
Thomas Nakagawa <thomasnakagawa@gmail.com>
2019-11-29 21:29:43 -0800

    Add settings menu

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OatsUtil;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        this.RequireDescendantGameObject("TypeWriterToggle").RequireComponent<Toggle>().isOn = PlayerPrefs.GetInt("TypewriterDisabled") == 0;
+    }
+
+    public void OnTypeWriterToggle(bool value)
+    {
+        PlayerPrefs.SetInt("TypewriterDisabled", value ? 0 : 1);
+    }
+}
